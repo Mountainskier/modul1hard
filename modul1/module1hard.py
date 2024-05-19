e19c30df5abc0f18591c0ba1b946973cf9dcbc2b
Mountain_skier <Slip6348@gmail.com>
2024-05-19 16:57:09 +0300

    changes

--- modul1/module1hard.py
+++ modul1/module1hard.py
@@ -1,9 +1,9 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-grades_x = 0
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students_list = sorted(students)
-students_x = 0
-for i in grades:
-    print({(students_list[students_x]): ((sum(grades[grades_x]) / len(grades[grades_x])))}, end=', ')
-    grades_x = grades_x + 1
-    students_x = students_x + 1
+student_grades = {}
+for student, grade in zip(students_list, grades):
+    average = sum(grade) / len(grade)
+    student_grades[student] = average
+print(student_grades)
+
